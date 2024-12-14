ed07bb8e36b26d77583c9d337d3fe4fd30106502
itsRamit <ece22145@iiitkalyani.ac.in>
2024-12-14 17:51:03 +0530

    Fix: Resolve API calling issue with proper JSON parsing and error handling

--- lib/constants/ApiConstants.dart
+++ lib/constants/ApiConstants.dart
@@ -1,3 +1,5 @@
 class API {
-  static const String baseURL = "https://polyjuice.kong.fampay.co/mock/famapp/feed/home_section/?slugs=famx-paypage";
+  static const String baseURL = 'polyjuice.kong.fampay.co';
+  static const String path = 'mock/famapp/feed/home_section/';
+  static const queryParam = {'slugs': 'famx-paypage'};
 }
